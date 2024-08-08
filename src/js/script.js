883ccbc4b59f6fdc5493bfd0971635d82fea1981
Daniil <neshadr1nsky@gmail.com>
2024-08-09 01:46:35 +0500

    reviews(item)

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,5 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+    slidesPerView: 'auto',
+    spaceBetween: 20,
 });
