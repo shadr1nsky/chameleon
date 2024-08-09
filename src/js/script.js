5cd8fb5bcc761a1888f4c2d45be7c96f6cb869a0
Daniil <neshadr1nsky@gmail.com>
2024-08-10 01:24:00 +0500

    reviews done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,26 @@
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiperCarousel', {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 56,
+    loop: true,
+    // autoplay: {
+    //     delay: 1000,
+    // },
+});
+
+var swiper = new Swiper('.swiperClients', {
     direction: 'horizontal',
     slidesPerView: 'auto',
     spaceBetween: 20,
+});
+
+var swiper = new Swiper('.swiperReviews', {
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
