816ac63919daee41a4549c5824b86f48a0070e16
Artem Shadrin <explay97@gmail.com>
2024-08-13 09:59:04 +0500

    faq

--- src/js/development.script.js
+++ src/js/development.script.js
@@ -1,3 +1,38 @@
+const initFaq = () => {
+    const faqElArray = document.querySelectorAll(".faq-item");
+    if (!faqElArray.length) {
+        return
+    }
+    faqElArray.forEach((faqEl => {
+        const faqButtonEl = faqEl.querySelector(".faq-item__button")
+        const faqAnswerEl = faqEl.querySelector(".faq-item__answer");
+        faqButtonEl.addEventListener("click", (() => {
+            console.log(faqEl.dataset.open)
+            if (faqEl.dataset.open === 'true') {
+                console.log('closing')
+                faqEl.dataset.open = "false";
+                faqEl.classList.remove("active");
+                faqAnswerEl.style.maxHeight = "";
+            } else {
+                faqElArray.forEach(oldFaqEl => {
+                    oldFaqEl.dataset.open = "false"
+                    oldFaqEl.classList.remove("active")
+                    oldFaqEl.querySelector(".faq-item__answer").style.maxHeight = ""
+                })
+                console.log('is-open')
+                faqEl.dataset.open = "true"
+                faqEl.classList.add("active");
+                faqAnswerEl.style.maxHeight = `${faqAnswerEl.scrollHeight}px`
+            }
+        }));
+        window.addEventListener("resize", (() => {
+            if (!!activeEl.dataset.open && parseInt(activeAnswerEl.style.maxHeight) !== activeAnswerEl.scrollHeight) {
+                activeAnswerEl.style.maxHeight = `${activeAnswerEl.scrollHeight}px`
+            }
+        }))
+    }))
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const servicesContainer = document.querySelectorAll("[data-services-item]");
     servicesContainer.forEach(service => {
@@ -58,6 +93,6 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 
-
+    initFaq()
 })
 
