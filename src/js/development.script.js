27cf6dbb550df0e6ee9ce7ea93f23b51716097e7
shadr1nsky <neshadr1nsky@gmail.com>
2024-08-23 22:50:27 +0500

    fix

--- src/js/development.script.js
+++ src/js/development.script.js
@@ -83,6 +83,23 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     })
 
+    let inputs = document.querySelectorAll('#feedback-button__input');
+    Array.prototype.forEach.call(inputs, function (input) {
+        let label = input.nextElementSibling,
+            labelVal = label.querySelector('#feedback-button__text').innerText;
+
+        input.addEventListener('change', function (e) {
+            let countFiles = '';
+            if (this.files && this.files.length >= 1)
+                countFiles = this.files.length;
+
+            if (countFiles)
+                label.querySelector('#feedback-button__text').innerText = 'Выбрано файлов: ' + countFiles;
+            else
+                label.querySelector('#feedback-button__text').innerText = labelVal;
+        });
+    });
+
     new Swiper('#swiperPromo', {
         direction: 'horizontal',
         slidesPerView: 1,
